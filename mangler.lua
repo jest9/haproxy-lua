e7cd812ada08fa5df854ef8f404f7442e6bcdda5
jest9 <max.radini@outlook.com>
2024-11-22 15:14:51 +0000

    added todo

--- mangler.lua
+++ mangler.lua
@@ -1,8 +1,7 @@
 
-
+-- uppercases set header Cookie on request
 core.register_action("uppercase_set_cookie", { "http-req" }, function(txn)
     -- get set-cookie header from response
-    -- txn.http:req_set_header("X-Custom-Header", "testres")
     local set_cookie_headers = txn.http:req_get_headers()["cookie"][0]
     local upper_cookie_headers = string.upper(set_cookie_headers)
     txn.http:req_set_header("Cookie", set_cookie_headers)
@@ -10,17 +9,12 @@ core.register_action("uppercase_set_cookie", { "http-req" }, function(txn)
 end)
 
 
--- Function to uppercase Set-Cookie header value
+-- lowercases cookie headers
 core.register_action("lowercase_set_cookie", { "http-res" }, function(txn)
     -- get set-cookie header from response
-    -- txn.http:res_set_header("X-Custom-Header", "testres")
     local set_cookie_headers = txn.http:res_get_headers()["set-cookie"][0]
     local lower_cookie_headers = string.lower(set_cookie_headers)
     txn.http:res_set_header("set-cookie", lower_cookie_headers)
 end)
 
--- TODO:
--- change cookie such that:
--- upper case on entry from haproxy
--- lowercase on exit from haproxy
--- cookie, Set-Cookie
+-- TODO: remove upper/lowercase test, add aes encryption/decryption of cookie
