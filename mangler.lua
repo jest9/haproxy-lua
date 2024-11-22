59e891b3ca76494bc0a460fb5f0dce7e003098ff
jest9 <max.radini@outlook.com>
2024-11-22 14:57:29 +0000

    changed a little

--- mangler.lua
+++ mangler.lua
@@ -1,27 +1,21 @@
---core.register_action("modify_set_cookie", { "http-req" }, function(txn)
-    -- Add custom header to the request
---   txn.http:req_set_header("X-Custom-Header", "testreq")
---end)
 
---core.register_action("add_custom_header", { "http-res" }, function(txn)
-    -- Add custom header to response
---    txn.http:res_set_header("X-Custom-Header", "testres")
---end)
+
 core.register_action("uppercase_set_cookie", { "http-req" }, function(txn)
     -- get set-cookie header from response
-    -- test : txn.http:res_set_header("X-Custom-Header", "testres")
+    -- txn.http:req_set_header("X-Custom-Header", "testres")
     local set_cookie_headers = txn.http:req_get_headers()["cookie"][0]
     local upper_cookie_headers = string.upper(set_cookie_headers)
-    txn.http:res_set_header("cookie", upper_cookie_headers)
+    txn.http:req_set_header("Cookie", set_cookie_headers)
+    -- txn.http:req_set_header("testres", "yes")
 end)
 
 
 -- Function to uppercase Set-Cookie header value
 core.register_action("lowercase_set_cookie", { "http-res" }, function(txn)
     -- get set-cookie header from response
-    -- test : txn.http:res_set_header("X-Custom-Header", "testres")
+    -- txn.http:res_set_header("X-Custom-Header", "testres")
     local set_cookie_headers = txn.http:res_get_headers()["set-cookie"][0]
-    local lower_cookie_headers = string.upper(set_cookie_headers)
+    local lower_cookie_headers = string.lower(set_cookie_headers)
     txn.http:res_set_header("set-cookie", lower_cookie_headers)
 end)
 
