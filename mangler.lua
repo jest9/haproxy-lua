854ed4a11bb7ae5d0fd7f8578a2f46203aedd522
jest9 <max.radini@outlook.com>
2024-11-28 15:19:06 +0000

    Added AES encryption for cookies in HAProxy Lua Script

--- mangler.lua
+++ mangler.lua
@@ -1,6 +1,8 @@
 local openssl = require("openssl")
-local aes = require "openssl".aes
+local evp = openssl.cipher.get('aes-128-gcm')
 local sha256 = openssl.digest.new("sha256")
+local info = evp:info()
+local tn = 16 -- tag
 
 -- hashing
 
@@ -15,47 +17,62 @@ function hashing(ip)
 end
 
 -- encryption logic
+e = evp:encrypt_new()
 
+function aes_encrypt(key, iv, msg, e, tn)
+    assert(e:ctrl(openssl.cipher.EVP_CTRL_GCM_SET_IVLEN, #iv))
+    assert(e:init(key, iv))
+    e:padding(false)
 
+    local c = assert(e:update(msg))
+    c = c .. e:final()
+    assert(#c==#msg)
 
+    local tag = assert(e:ctrl(openssl.cipher.EVP_CTRL_GCM_GET_TAG, tn))
+   assert(#tag==tn)
 
-
-
-
-
-
-
+    return c, tag
+end
 
 
 
 
 -- haproxy logic
 
-core.register_action("encrypt_set_cookie", { "http-req" }, function(txn)
+-- cookie decrypted on request
+
+core.register_action("decrypt_set_cookie", { "http-req" }, function(txn)
     local set_cookie_headers = txn.http:req_get_headers()["cookie"][0]
-    local remote_addr = txn.sf:src()
-    local key, iv = hashing(remote_addr)
     local upper_cookie_headers = string.upper(set_cookie_headers)
     --test block
     
 
-    txn.http:req_set_header("Cookie", key)
+    -- txn.http:req_set_header("Cookie", key)
 
     -- txn.http:req_set_header("Cookie", upper_cookie_headers)
 
 end)
 
--- lowercase set cookie
-core.register_action("decrypt_set_cookie", { "http-res" }, function(txn)
+
+-- cookie encrypted on response
+
+core.register_action("encrypt_set_cookie", { "http-res" }, function(txn)
     local set_cookie_headers = txn.http:res_get_headers()["set-cookie"][0]
-    local lower_cookie_headers = string.lower(set_cookie_headers)
-    
+    -- local lower_cookie_headers = string.lower(set_cookie_headers)
+    local remote_addr = txn.sf:src()
+    local key, iv = hashing(remote_addr)
+    local encrypted_cookie, tag = aes_encrypt(key, iv, set_cookie_headers, e, tn)
+    local enc64 = openssl.base64(encrypted_cookie)
+
     -- test block
     -- local test = openssl.version()
-    txn.http:res_set_header("set-cookie", lower_cookie_headers)    
+    txn.http:res_set_header("set-cookie", enc64)    
 
     -- txn.http:res_set_header("set-cookie", lower_cookie_headers)
 end)
 
 
 -- todo: encrypt cookies
+-- todo: check for valid cookie header
+-- todo: 
+-- todo: comments and refactor
